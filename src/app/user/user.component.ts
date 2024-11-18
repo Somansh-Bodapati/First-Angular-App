4d5f5e6756baadc268c60633647f44c995b7a86e
Somansh-Bodapati <somansh.bodapati@gmail.com>
2024-11-18 16:14:15 -0600

    Listing all users and now working on tasks

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,12 +1,29 @@
-import { Component } from '@angular/core';
+import {Component, computed, Input, input, Output, EventEmitter } from '@angular/core';
+
+import { DUMMY_USERS } from './users';
+import {NgOptimizedImage} from '@angular/common';
 
 @Component({
   selector: 'app-user',
   standalone: true,
-  imports: [],
+  imports: [
+    NgOptimizedImage
+  ],
   templateUrl: './user.component.html',
   styleUrl: './user.component.css'
 })
 export class UserComponent {
+  @Input({ required:true }) id!: string;
+  @Input({ required:true }) avatar!: string;
+  @Input({ required:true }) name!: string;
+  @Output() select = new EventEmitter();
 
+
+  get imagePath(){
+    return 'assets/users/' + this.avatar
+  }
+
+  onSelectUser(){
+    this.select.emit(this.id);
+  }
 }

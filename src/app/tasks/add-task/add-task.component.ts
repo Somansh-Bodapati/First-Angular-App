2bbb61a812b5078e76d7613923acabdfadc33bce
Somansh-Bodapati <somansh.bodapati@gmail.com>
2024-11-21 11:48:56 -0600

    Created two way binding ngModels to store info entered while adding task.

--- src/app/tasks/add-task/add-task.component.ts
+++ src/app/tasks/add-task/add-task.component.ts
@@ -1,14 +1,20 @@
 import {Component, EventEmitter, Input, Output} from '@angular/core';
+import {FormsModule} from "@angular/forms";
 
 @Component({
   selector: 'app-add-task',
   standalone: true,
-  imports: [],
+  imports: [
+    FormsModule
+  ],
   templateUrl: './add-task.component.html',
   styleUrl: './add-task.component.css'
 })
 export class AddTaskComponent {
   @Output() cancel = new EventEmitter<string>();
+  enteredTitle = '';
+  enteredSummary = '';
+  enteredDate = '';
 
   onCancel(){
     this.cancel.emit();
